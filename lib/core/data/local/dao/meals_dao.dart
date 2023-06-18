3924f9233fff91827dc68d0c67e24a15d1bedb4a
Robaa <njuguiinirobert@gmail.com>
2023-06-18 09:55:21 +0300

    setting up local caching

---
+++ lib/core/data/local/dao/meals_dao.dart
@@ -0,0 +1,18 @@
+import 'package:DishDash/feature/saved/data/datasource/local/entity/meal_entity.dart';
+import 'package:floor/floor.dart';
+
+@dao
+abstract class MealsDao {
+  @Insert(onConflict: OnConflictStrategy.replace)
+  Future<void> insertToFavorites(MealEntity collection);
+
+  @Query('SELECT * FROM meal')
+  Future<List<MealEntity>> getAllFavorites();
+
+  @Query('DELETE FROM meal')
+  Future<void> deleteAllFavorites();
+
+  @Query('DELETE FROM meal WHERE id = :id')
+  Future<void> deleteById(String id);
+
+}
