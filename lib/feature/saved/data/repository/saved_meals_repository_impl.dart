2d631cf0f32f0f79572fabfe609762be284c1a25
Robaa <njuguiinirobert@gmail.com>
2023-06-19 15:13:35 +0300

    working on saved meals data layer

---
+++ lib/feature/saved/data/repository/saved_meals_repository_impl.dart
@@ -0,0 +1,34 @@
+
+import 'package:DishDash/core/errors/exceptions.dart';
+import 'package:DishDash/core/errors/failures.dart';
+import 'package:DishDash/feature/saved/data/datasource/saved_meals_local_data_source.dart';
+import 'package:DishDash/feature/saved/data/mappers/mappers.dart';
+import 'package:DishDash/feature/saved/domain/models/saved_food_model.dart';
+import 'package:DishDash/feature/saved/domain/repository/saved_meals_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class SavedMealsRepositoryImpl implements SavedMealsRepository {
+  final SavedMealsLocalDataSource dataSource;
+  SavedMealsRepositoryImpl(this.dataSource);
+
+  @override
+  Future<Either<Failure, List<SavedFoodModel>>> getSavedMeals() async {
+    try{
+      final response = await dataSource.getAllSavedMeals();
+      final results = response.map((e) => e.toModel()).toList();
+      return Right(results);
+    } on DatabaseException catch(e){
+      return Left(DatabaseFailure(e.message));
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> deleteSavedMeal(String mealId) async {
+    try{
+      await dataSource.deleteSavedMeal(mealId);
+      return const Right(null);
+    } on DatabaseException catch(e){
+      return Left(DatabaseFailure(e.message));
+    }
+  }
+}
---
+++ lib/feature/saved/domain/repository/saved_meals_repository.dart
@@ -0,0 +1,10 @@
+
+
+import 'package:DishDash/core/errors/failures.dart';
+import 'package:DishDash/feature/saved/domain/models/saved_food_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class SavedMealsRepository {
+  Future<Either<Failure, List<SavedFoodModel>>> getSavedMeals();
+  Future<Either<Failure, void>> deleteSavedMeal(String mealId);
+}
