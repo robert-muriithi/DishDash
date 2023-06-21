783922b9572d383a9aa21d32ee63759608182c00
Robaa <njuguiinirobert@gmail.com>
2023-06-21 22:07:11 +0300

    additional screens

--- lib/feature/saved/presentation/page/saved_page.dart
+++ lib/feature/saved/presentation/page/saved_page.dart
@@ -1,14 +1,75 @@
+import 'package:DishDash/core/di/injector_container.dart';
+import 'package:DishDash/core/utils/utils.dart';
+import 'package:DishDash/feature/saved/domain/models/saved_food_model.dart';
+import 'package:DishDash/feature/saved/presentation/cubits/saved_meals_cubit.dart';
+import 'package:DishDash/feature/saved/presentation/page/saved_meal_details.dart';
+import 'package:DishDash/feature/saved/presentation/widgets/saved_meal_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SavedPage extends StatelessWidget {
   const SavedPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Saved Screen"),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => sl<SavedMealsCubit>()..getSavedMeals(),
+        ),
+      ],
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Saved Meals"),
+        ),
+        body: _buildBody(context),
       ),
+    );
+  }
+
+  BlocConsumer<SavedMealsCubit, SavedMealsState> _buildBody(BuildContext context) {
+    return BlocConsumer<SavedMealsCubit, SavedMealsState>(
+      listener: (context, state) {
+        if (state.uiState == UIState.error) {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text(state.errorMessage ?? "Something went wrong"),
+          ));
+        }
+      },
+      builder: (context, state) {
+        if (state.uiState == UIState.loading) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (state.uiState == UIState.success) {
+          return ListView.builder(
+            itemCount: state.savedMeals?.length,
+            itemBuilder: (context, index) {
+              final meal = state.savedMeals?[index] ?? SavedFoodModel();
+              return InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SavedMealDetailsPage(
+                        meal: meal,
+                      ),
+                    ),
+                  );
+                },
+                child: SavedMealItem(
+                  meal: meal,
+                  onDelete: (String mealId) {
+                    context.read<SavedMealsCubit>().deleteSavedMeal(mealId);
+                  },
+                ),
+              );
+            },
+          );
+        } else {
+          return const SizedBox.shrink();
+        }
+      },
     );
   }
 }
--- lib/feature/settings/presentation/page/settings_page.dart
+++ lib/feature/settings/presentation/page/settings_page.dart
@@ -1,6 +1,6 @@
-import 'package:DishDash/core/presentation/widgets/text/small_text.dart';
+import 'package:DishDash/config/theme/colors.dart';
 import 'package:flutter/material.dart';
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -9,10 +9,259 @@ class SettingsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Settings Screen"),
+        title: const Text("Settings"),
       ),
-      body:  Center(
-        child: Container()
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        physics: const AlwaysScrollableScrollPhysics(),
+        child: Column(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(children: [
+                  Column(
+                    children: const [
+                      CircleAvatar(
+                        backgroundColor: Colors.transparent,
+                        radius: 30,
+                        child: Image(
+                          image: AssetImage(
+                            'assets/images/wallpaper.jpg',
+                          ),
+                          width: 50,
+                          height: 50,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(width: 10),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'John Doe',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          '+254677834389',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.black.withOpacity(0.5),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Spacer(),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(FontAwesomeIcons.penToSquare,
+                        color: AppColors.lightColorScheme.primary, size: 18),
+                  ),
+                ]),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10, right: 20),
+                child: Row(children: const [
+                  Text(
+                    'Account',
+                    style: TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                ]),
+              ),
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 0),
+                child: Column(
+                  children: [
+                    Material(
+                      type: MaterialType.transparency,
+                      child: ListTile(
+                        visualDensity:
+                            const VisualDensity(horizontal: 0, vertical: -4),
+                        leading: Icon(FontAwesomeIcons.wallet,
+                            color: AppColors.lightColorScheme.primary,
+                            size: 18),
+                        title: const Text(
+                          'Wallet',
+                          style: TextStyle(fontSize: 16, color: Colors.black),
+                        ),
+                        subtitle: Text(
+                          'My earnings',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.black.withOpacity(0.5),
+                          ),
+                        ),
+                        onTap: () {},
+                      ),
+                    ),
+                    const Divider(
+                      indent: 20,
+                      endIndent: 20,
+                    ),
+                    Material(
+                      type: MaterialType.transparency,
+                      child: ListTile(
+                        visualDensity:
+                            const VisualDensity(horizontal: 0, vertical: -4),
+                        leading: Icon(FontAwesomeIcons.clockRotateLeft,
+                            color: AppColors.lightColorScheme.primary,
+                            size: 18),
+                        title: const Text(
+                          'History',
+                          style: TextStyle(fontSize: 16, color: Colors.black),
+                        ),
+                        subtitle: Text(
+                          'View history',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.black.withOpacity(0.5),
+                          ),
+                        ),
+                        onTap: () {},
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10, right: 20),
+                child: Row(children: const [
+                  Text(
+                    'Support',
+                    style: TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                ]),
+              ),
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Column(
+                  children: [
+                    Material(
+                      type: MaterialType.transparency,
+                      child: ListTile(
+                        visualDensity:
+                            const VisualDensity(horizontal: 0, vertical: -3),
+                        leading: Icon(FontAwesomeIcons.weixin,
+                            color: AppColors.lightColorScheme.primary,
+                            size: 18),
+                        title: const Text(
+                          'Feedback',
+                          style: TextStyle(fontSize: 16, color: Colors.black),
+                        ),
+                        subtitle: Text(
+                          'Tal to us today',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.black.withOpacity(0.5),
+                          ),
+                        ),
+                        onTap: () {},
+                      ),
+                    ),
+                    const Divider(
+                      indent: 20,
+                      endIndent: 20,
+                    ),
+                    Material(
+                      type: MaterialType.transparency,
+                      child: ListTile(
+                        visualDensity:
+                            const VisualDensity(horizontal: 0, vertical: -3),
+                        leading: Icon(FontAwesomeIcons.circleInfo,
+                            color: AppColors.lightColorScheme.primary,
+                            size: 18),
+                        title: const Text(
+                          'About Us',
+                          style: TextStyle(fontSize: 16, color: Colors.black),
+                        ),
+                        subtitle: Text(
+                          'Learn and know more about us',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.black.withOpacity(0.5),
+                          ),
+                        ),
+                        onTap: () {},
+                      ),
+                    ),
+                    const Divider(
+                      indent: 20,
+                      endIndent: 20,
+                    ),
+                    Material(
+                      type: MaterialType.transparency,
+                      child: ListTile(
+                        leading: Icon(FontAwesomeIcons.arrowRightFromBracket,
+                            color: AppColors.lightColorScheme.primary,
+                            size: 18),
+                        title: const Text(
+                          'Logout',
+                          style: TextStyle(fontSize: 16, color: Colors.red),
+                        ),
+                        subtitle: Text(
+                          'Logout from your account',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.black.withOpacity(0.5),
+                          ),
+                        ),
+                        onTap: () {},
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 20),
+                          child: Column(
+                            children: [
+                              Text(
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.black.withOpacity(0.5),
+                                  ),
+                                  'Version 1.0.0'),
+                              Text(
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.black.withOpacity(0.5),
+                                  ),
+                                  'DishDash © 2023'),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
